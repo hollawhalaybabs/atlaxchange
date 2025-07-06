7642b64608e2c252ed3f0da0d2c659e4e9f07c65
hollawhalaybabs <hollawhalaybabs@gmail.com>
2025-07-07 00:12:56 +0100

    Readonly

--- fetch_users_api/models/account_ledger.py
+++ fetch_users_api/models/account_ledger.py
@@ -5,6 +5,6 @@ class AccountLedger(models.Model):
     _description = 'Account Ledger'
 
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    currency_id = fields.Many2one('supported.currency', string='Currency', required=True)
-    wallet_id = fields.Char(string='Wallet ID', required=True)
-    balance = fields.Float(string='Balance', required=True)
+    currency_id = fields.Many2one('supported.currency', string='Currency', required=True, readonly=True)
+    wallet_id = fields.Char(string='Wallet ID', required=True, readonly=True)
+    balance = fields.Float(string='Balance', required=True, readonly=True)
